c01733285f73b3cd54527f5c4c250972c6e3af15
JonathanRVega <Jonathaan.rv@gmail.com>
2020-12-01 11:14:08 -0700

    added new component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar from "./components/navbar.component";
+import AttractionList from "./components/attractions-list.component";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      
+      <Route path="/" component={AttractionList} />
     </Router>
   );
 }
