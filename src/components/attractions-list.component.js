8c34c5f34d53f2a300826f663f2c69b6fe362373
JonathanRVega <Jonathaan.rv@gmail.com>
2020-12-03 07:14:21 -0700

    added cards

--- src/components/attractions-list.component.js
+++ src/components/attractions-list.component.js
@@ -15,7 +15,7 @@ export default class AttractionList extends Component {
     constructor(props) {
         super (props);
 
-        this.state = {attractions: []};
+		this.state = {attractions: []};
     }
 
     componentDidMount() {
@@ -32,8 +32,10 @@ export default class AttractionList extends Component {
         return this.state.attractions.map(currentattraction => {
           return <Attraction attraction={currentattraction} key={currentattraction._id}/>;
         })
-      }
+	  }
 
+
+	  
     render() {
         return (
             <div>
@@ -52,6 +54,64 @@ export default class AttractionList extends Component {
                 { this.attractionList() }
                 </tbody>
             </table>
+
+            <div class="container">
+	<div class="row">
+		<div class="col">
+			<div class="weather-card one">
+				<div class="top">
+					<div class="wrapper">
+						<div class="mynav">
+							<a href="javascript:;"><span class="lnr lnr-chevron-left"></span></a>
+							<a href="javascript:;"><span class="lnr lnr-cog"></span></a>
+						</div>
+						<h1 class="heading">Tokyo Kitty</h1>
+						<h3 class="location">84.51 building, 575 Race St, Cincinnati, OH, 45202</h3>
+					</div>
+				</div>
+				<div class="bottom">
+					<div class="wrapper">
+						<ul class="forecast">
+							<a href="javascript:;"><span class="lnr lnr-chevron-up go-up"></span></a>
+							<li class="active">
+							<p className="cardText">Cool Japanese-style karaoke bar with cocktails, dancing & private rooms with lounge seating.</p>
+							<p className="cardText">Website: <span><a href="">https://thattokyobar.com/</a></span></p>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="col">
+			<div class="weather-card rain">
+				<div class="top">
+					<div class="wrapper">
+						<div class="mynav">
+							<a href="javascript:;"><span class="lnr lnr-chevron-left"></span></a>
+							<a href="javascript:;"><span class="lnr lnr-cog"></span></a>
+						</div>
+						<h1 class="heading">Nada</h1>
+						<h3 class="location">1331 Walnut St, Cincinnati, OH, 45202</h3>
+					</div>
+				</div>
+				<div class="bottom">
+					<div class="wrapper">
+					<ul class="forecast">
+							<a href="javascript:;"><span class="lnr lnr-chevron-up go-up"></span></a>
+							<li class="active">
+							<p className="cardText">Trendy Mexican cantina with outdoor seating, festive cocktails & upscale fare in a chic setting.</p>
+							<p className="cardText">Website: <span><a href="">https://www.eatdrinknada.com/</a></span></p>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</div>
+
             </div>
         )
     }
