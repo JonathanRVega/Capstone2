6156695ba1178b73254733814330b356f2449fc8
Dan S <sachsd1116@outlook.com>
2020-12-03 15:16:33 -0500

    Added Navbar component  to Contact component

--- src/components/Contact.component.js
+++ src/components/Contact.component.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import CreateMail from './create-mail.component'
+import Navbar from './navbar.component'
 
 export default class Contact extends Component {
     render() {
         return (
             <div>
+                <Navbar/>
                 <CreateMail />
             </div>
         )
