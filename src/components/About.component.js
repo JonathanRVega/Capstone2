30594645bf7d64967f02ac9ba95ee824d082a53a
Dan S <sachsd1116@outlook.com>
2020-12-07 15:10:43 -0500

    Not ready to push responsive css changes

--- src/components/About.component.js
+++ src/components/About.component.js
@@ -27,7 +27,7 @@ import wholeAgain from '../assets/charities/WholeAgainlogotaglineRGB-web-logo.pn
                {/* end of header section - cc */}
                <div className="about container mt-5">
                   <div className="row">
-                     <div className="col-sm-6 mt-5">
+                     <div className="col-sm-12 col-md-6 mt-5">
                         <div className="line"></div>
                         <h2>Our</h2><br/>
                         <h1>Mission</h1> 
@@ -36,19 +36,19 @@ import wholeAgain from '../assets/charities/WholeAgainlogotaglineRGB-web-logo.pn
                            Nulla non tincidunt metus, ac lobortis libero. Suspendisse dictum ante eu enim iaculis, <br/>
                            eget ultrices est hendrerit.</p>
                      </div>
-                     <div className="col-sm-2"></div>
-                     <div className="col-sm-4">
-                        <div className="aboutImageBackground ml-5 float-right"></div>
-                        <div className="aboutImage mr-5 float-right" id="aboutImage1"></div>
+                     <div className="col-md-2 .d-none .d-sm-none .d-md-block"></div>
+                     <div className="col-sm-12 col-md-4">
+                        <div className="aboutImageBackground ml-5 float-sm-right"></div>
+                        <div className="aboutImage mr-5 float-sm-right" id="aboutImage1"></div>
                      </div>
                   </div>
                   <div className="row mt-5">   
-                     <div className="col-sm-4">
+                     <div className="col-sm-12 col-md-4">
                         <div className="aboutImageBackground"></div>
                         <div className="aboutImage ml-5" id="aboutImage2"></div>
                      </div>
-                     <div className="col-sm-2"></div>
-                     <div className="col-sm-6 mt-5">
+                     <div className="col-md-2 .d-none .d-sm-none .d-md-block"></div>
+                     <div className="col-sm-12 col-md-6 mt-5">
                         <div className="line"></div>
                         <h2>Meet</h2><br/>
                         <h1>The Team</h1> 
@@ -63,10 +63,10 @@ import wholeAgain from '../assets/charities/WholeAgainlogotaglineRGB-web-logo.pn
                      <div className="col-sm-12 charitiesBackground mt-5 py-5 text-center">
                         <h2>Explore Cincinnati Charities</h2>
                         <a href="https://www.marchofdimes.org/"><img style={{width:'10rem'}} src={marchOfDimes}/></a>
-                        <a href="https://www.sparksfoundation.org/support-us/?gclid=Cj0KCQiAk53-BRD0ARIsAJuNhpsemiNo_z0291QTu9j143jWLNWeTOtoVZCYEHYaxe0Nkz8JKrLtTugaAjKgEALw_wcB"><img style={{width:'20rem'}} src={sparks}/></a>
-                        <a href="https://rmhc-centralohio.org/?gclid=Cj0KCQiAk53-BRD0ARIsAJuNhptorRD_1Vt7lPKqNZGXhXynOGJaaRnFi9m0HMN3cb7DNIyqEkVNjDgaAiLlEALw_wcB"><img style={{width:'20rem'}} src={ronaldMcDonaldHouse}/></a>
-                        <a href="https://www.achildshopefoundation.org/?gclid=Cj0KCQiAk53-BRD0ARIsAJuNhpvOh5HEcgHlzCUOp3cQHkX97fhs5LWAmU9yiBjeNs71OXN1m-6SNOMaAsvZEALw_wcB"><img style={{width:'20rem'}} src={aChildsHope}/></a>
-                        <a href="https://www.whole-again.org/"><img style={{width:'20rem'}} src={wholeAgain}/></a>
+                        <a href="https://www.sparksfoundation.org/support-us/?gclid=Cj0KCQiAk53-BRD0ARIsAJuNhpsemiNo_z0291QTu9j143jWLNWeTOtoVZCYEHYaxe0Nkz8JKrLtTugaAjKgEALw_wcB"><img id="aboutLogos" src={sparks}/></a>
+                        <a href="https://rmhc-centralohio.org/?gclid=Cj0KCQiAk53-BRD0ARIsAJuNhptorRD_1Vt7lPKqNZGXhXynOGJaaRnFi9m0HMN3cb7DNIyqEkVNjDgaAiLlEALw_wcB"><img id="aboutLogos" src={ronaldMcDonaldHouse}/></a>
+                        <a href="https://www.achildshopefoundation.org/?gclid=Cj0KCQiAk53-BRD0ARIsAJuNhpvOh5HEcgHlzCUOp3cQHkX97fhs5LWAmU9yiBjeNs71OXN1m-6SNOMaAsvZEALw_wcB"><img id="aboutLogos" src={aChildsHope}/></a>
+                        <a href="https://www.whole-again.org/"><img id="aboutLogos" src={wholeAgain}/></a>
                      </div>
                   </div>
             </div>
