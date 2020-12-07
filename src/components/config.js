e6e55c0547aa0bff50fe9c86c0fea94f3054b455
JonathanRVega <Jonathaan.rv@gmail.com>
2020-12-07 14:15:50 -0700

    heroku deployment

--- src/components/config.js
+++ src/components/config.js
@@ -3,5 +3,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 export const BACKEND_URL =
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8080/'
+    ? 'http://localhost:5000/'
     : 'https://capstonecincinnati.herokuapp.com/';
