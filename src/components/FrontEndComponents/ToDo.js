5dc2e04c8917870e0f8dad79dacea1cc4c4a47c1
JonathanRVega <Jonathaan.rv@gmail.com>
2020-12-10 09:52:55 -0700

    fully responsive things to do page

--- src/components/FrontEndComponents/ToDo.js
+++ src/components/FrontEndComponents/ToDo.js
@@ -31,9 +31,9 @@ import Asm from '../../assets/places/asm.jpg';
                     
                 </header>
                
-                <section className="facts container">
-                     <div className="row container containerBackgroundBig">
-                        <div className="col-lg-4 facts-text-box">
+                <section>
+                     <div className="containerRow containerBackgroundBig">
+                        <div className="containerColumn facts-text-box">
                             <div className="line"></div>
                                <h1>Explore<br/>The City Life</h1>
                                <p className="todoJustify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui vel lectus efficitur rutrum.
@@ -43,87 +43,78 @@ import Asm from '../../assets/places/asm.jpg';
                                </p>
                               </div>
 
-                              <div className="col-8">
-                              <div class="gallery">
-                              <figure class="gallery__item gallery__item--1">
-                                 <img src={TokyoKitty} class="gallery__img" alt="Tokyo Kitty"></img>
-                              </figure>
-                              <figure class="gallery__item gallery__item--2">
-                                 <img src={Taste} class="gallery__img" alt="Taste"></img>
-                              </figure>
-                              <figure class="gallery__item gallery__item--3">
-                                 <img src={Waterview} class="gallery__img" alt="Waterview"></img>
-                              </figure>
-                              <figure class="gallery__item gallery__item--4">
-                                 <img src={Zoo} class="gallery__img" alt="Zoo of Cincinnati"></img>
-                              </figure>
-                              <figure class="gallery__item gallery__item--6">
-                                 <img src={Asm} class="gallery__img" alt="ASM"></img>
-                              </figure>
-                              </div>
+                              <div className="containerColumn">
+                                 <div class="gallery">
+                                 <figure class="gallery__item gallery__item--1">
+                                    <img src={TokyoKitty} class="gallery__img" alt="Tokyo Kitty"></img>
+                                 </figure>
+                                 <figure class="gallery__item gallery__item--2">
+                                    <img src={Taste} class="gallery__img" alt="Taste"></img>
+                                 </figure>
+                                 <figure class="gallery__item gallery__item--3">
+                                    <img src={Waterview} class="gallery__img" alt="Waterview"></img>
+                                 </figure>
+                                 <figure class="gallery__item gallery__item--4">
+                                    <img src={Zoo} class="gallery__img" alt="Zoo of Cincinnati"></img>
+                                 </figure>
+                                 <figure class="gallery__item gallery__item--6">
+                                    <img src={Asm} class="gallery__img" alt="ASM"></img>
+                                 </figure>
+                                 </div>
                               </div>  
-                              </div>                                                                            
+                        </div>                                                                            
                 </section>
 
 
-                <section className="facts container">
-                     <div className="row containerBackground">
-                     
-
-                        <div className="col-lg-6" id='smol'>
-                           
-                                    <img className="todo-img" src={ Reds } alt="Great American Ball Park" />
-                                    </div>  
-
-                                         <div className="col-lg-6 facts-text-box">
-                            <div className="line"></div>
-                               <h1>Shop<br/>The City Life</h1>
-                               <p className="todoJustify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui vel lectus efficitur rutrum.
-                                  Nullam facilisis lorem in enim bibendum interdum.
-                                  Nulla non tincidunt metus, ac lobortis libero. Suspendisse dictum ante eu enim iaculis,
-                                  eget ultrices est hendrerit.
-                               </p>
-                              </div>                            
-                                </div>                                           
+                <section>
+                  <div className="containerRow containerBackground">
+                     <div className="containerColumn">
+                     <img className="todo-img" src={ Reds } alt="Great American Ball Park" />
+                     </div>
+                     <div className="containerColumn facts-text-box">
+                     <div className="line"></div>
+                     <h1 className="factsHeader">Shop<br/>The City Life</h1>
+                     <p className="todoJustify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui vel lectus efficitur rutrum.
+                        Nullam facilisis lorem in enim bibendum interdum.
+                        Nulla non tincidunt metus, ac lobortis libero. Suspendisse dictum ante eu enim iaculis,
+                        eget ultrices est hendrerit.
+                     </p>
+                     </div>
+                  </div>
                 </section>
-                <section className="facts container">
-                     <div className="row containerBackground">
-                        <div className="col-lg-6 facts-text-box">
-                            <div className="line"></div>
-                               <h1>Eat<br/>The City Life</h1>
-                               <p className="todoJustify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui vel lectus efficitur rutrum.
-                                  Nullam facilisis lorem in enim bibendum interdum.
-                                  Nulla non tincidunt metus, ac lobortis libero. Suspendisse dictum ante eu enim iaculis,
-                                  eget ultrices est hendrerit.
-                               </p>
-                              </div>
 
-                        <div className="col-lg-6" id='smol'>
-                           
-                                    <img className="todo-img" src={ NadaOutside } alt="Nada Exterior" />
-                                    </div>                                
-                                </div>                                           
-                </section>
-                <section className="facts container">
-                     <div className="row containerBackground">
-                       
-                        <div className="col-lg-6" id='smol'>
-                           
-                                    <img className="todo-img" src={ TKGodzilla } alt="Karoke Room at Tokyo Kitty" />
-                                    </div> 
-
-                                      <div className="col-lg-6 facts-text-box">
-                              
-                            <div className="line"></div>
-                               <h1>Play<br/>The City Life</h1>
-                               <p className="todoJustify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui vel lectus efficitur rutrum.
-                                  Nullam facilisis lorem in enim bibendum interdum.
-                                  Nulla non tincidunt metus, ac lobortis libero. Suspendisse dictum ante eu enim iaculis,
-                                  eget ultrices est hendrerit.
-                               </p>
+                <section>                                        
+                  <div className="containerRow containerBackground">
+                     <div className="containerColumn facts-text-box">
+                        <div className="line"></div>
+                        <h1 className="factsHeader">Eat<br/>The City Life</h1>
+                        <p className="todoJustify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui vel lectus efficitur rutrum.
+                           Nullam facilisis lorem in enim bibendum interdum.
+                           Nulla non tincidunt metus, ac lobortis libero. Suspendisse dictum ante eu enim iaculis,
+                           eget ultrices est hendrerit.
+                        </p>
                         </div>
-                              
-                     </div>                                           
+                     <div className="containerColumn">
+                        <img className="todo-img" src={ NadaOutside } alt="NadaExterior" />
+                     </div>
+               </div>
+                </section>
+
+                <section>
+                  <div className="containerRow containerBackground">
+                     <div className="containerColumn">
+                        <img className="todo-img" src={ TKGodzilla } alt="Great American Ball Park" />
+                     </div>
+                     <div className="containerColumn facts-text-box">
+                        <div className="line"></div>
+                        <h1 className="factsHeader">Play<br/>The City Life</h1>
+                        <p className="todoJustify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dui vel lectus efficitur rutrum.
+                           Nullam facilisis lorem in enim bibendum interdum.
+                           Nulla non tincidunt metus, ac lobortis libero. Suspendisse dictum ante eu enim iaculis,
+                           eget ultrices est hendrerit.
+                        </p>
+                     </div>
+                  </div>
                 </section>
 
 
