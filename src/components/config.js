c7bf9abf5b596514315aa33db09786f9925d43fe
JonathanRVega <Jonathaan.rv@gmail.com>
2020-12-14 13:43:09 -0700

    duh

--- src/components/config.js
+++ src/components/config.js
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 export const BACKEND_URL =
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5000/'
+    ? 'http://localhost:8080/'
     : 'https://capstoneprojectcincinnati.herokuapp.com/';
  
