18c59233d2ea9f2779c82935d93a0361908d09cb
JonathanRVega <Jonathaan.rv@gmail.com>
2020-12-14 07:45:57 -0700

    took out image

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
-//app.use(bodyParser.urlencoded({ extended: true}));
+//app.use(bodyParser.urlencoded({ extended: true})a);
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
